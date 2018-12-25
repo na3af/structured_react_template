4271de15ae93233d7b98d1e99ca659eaae610023
Nawaf Khalid Alhumaid <naalhumaid@elm.com.sa>
2018-12-25 11:06:16 +0300

    add react template Files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import MyApp from "./containers/App/App";
+
+class App extends Component {
+  render() {
+    return <MyApp />;
+  }
+}
+
+export default App;
---
+++ src/containers/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return <div>jh</div>;
+  }
+}
+
+export default App;
